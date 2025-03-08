5cf5f29ce0775baa815f96a50b8fe98ce1711ddf
NeonShark1043 <108032761+NeonShark1043@users.noreply.github.com>
2025-03-07 22:42:20 -0500

    Add files via upload

---
+++ Imports56.py
@@ -0,0 +1,22 @@
+# Shared imports for Team 56 Project 3
+
+import time
+from buildhat import Motor
+from basehat import IMUSensor
+from basehat import UltrasonicSensor
+from basehat import HallSensor
+from basehat import IRSensor
+from enum import Enum
+
+__all__ = ['time', 'Motor', 'IMUSensor', 'UltrasonicSensor',
+           'HallSensor', 'IRSensor', 'Enum']
+           
+'''
+import time
+from buildhat import Motor
+from basehat import IMUSensor
+from basehat import UltrasonicSensor
+from basehat import HallSensor
+from basehat import IRSensor
+from enum import Enum
+'''
